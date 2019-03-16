7631a4f18cdc5df28c7cc72ff31caf4058ff0924
Robin Allen <r@foon.uk>
2019-03-16 11:17:28 +0000

    Fix modules/js.js when using module.exports
    pl.type was being accessed before it was defined

--- modules/js.js
+++ modules/js.js
@@ -2,6 +2,90 @@ var pl;
 (function( pl ) {
 
 	var predicates = function() {
+
+		// JS OBJECTS
+
+		// Is a JS object
+		pl.type.is_js_object = function( obj ) {
+			return obj instanceof pl.type.JSValue;
+		};
+
+		// Ordering relation
+		pl.type.order.push( pl.type.JSValue );
+
+		// JSValue Prolog object
+		pl.type.JSValue = function( value ) {
+			this.value = value;
+		}
+
+		// toString
+		pl.type.JSValue.prototype.toString = function() {
+			return "JSValue" + (typeof this.value).toUpperCase();
+		};
+
+		// clone
+		pl.type.JSValue.prototype.clone = function() {
+			return new pl.type.JSValue( this.value );
+		};
+
+		// equals
+		pl.type.JSValue.prototype.equals = function( obj ) {
+			return pl.type.is_js_object( obj ) && this.value === obj.value;
+		};
+
+		// rename
+		pl.type.JSValue.prototype.rename = function( _ ) {
+			return this;
+		};
+
+		// get variables
+		pl.type.JSValue.prototype.variables = function() {
+			return [];
+		};
+
+		// apply substitutions
+		pl.type.JSValue.prototype.apply = function( _ ) {
+			return this;
+		};
+
+		// unify
+		pl.type.JSValue.prototype.unify = function( obj, _ ) {
+			if( pl.type.is_js_object( obj ) && this.value === obj.value ) {
+				return new pl.type.State( obj, new pl.type.Substitution() );
+			}
+			return null;
+		};
+
+		// interpret
+		pl.type.JSValue.prototype.interpret = function( indicator ) {
+			return pl.error.instantiation( indicator );
+		};
+
+		// compare
+		pl.type.JSValue.prototype.compare = function( obj ) {
+			if( this.value === obj.value ) {
+				return 0;
+			} else if( this.value < obj.value ) {
+				return -1;
+			} else if( this.value > obj.value ) {
+				return 1;
+			}
+		};
+		
+		// to javascript
+		pl.type.JSValue.prototype.toJavaScript = function() {
+			return this.value;
+		};
+		
+		// from javascrip
+		pl.fromJavaScript.conversion.any = function( obj ) {
+			return new pl.type.JSValue( obj );
+		};
+		
+		// JavaScript error
+		pl.error.javascript = function( error, indicator ) {
+			return new pl.type.Term( "error", [new pl.type.Term( "javascript_error", [new pl.type.Term( error )] ), pl.utils.str_indicator( indicator )] );
+		};
 		
 		return {
 			
@@ -100,95 +184,6 @@ var pl;
 	
 	var exports = ["global/1", "apply/3", "apply/4", "prop/2", "prop/3"];
 
-
-
-	// JS OBJECTS
-	
-	// Is a JS object
-	pl.type.is_js_object = function( obj ) {
-		return obj instanceof pl.type.JSValue;
-	};
-
-	// Ordering relation
-	pl.type.order.push( pl.type.JSValue );
-
-	// JSValue Prolog object
-	pl.type.JSValue = function( value ) {
-		this.value = value;
-	}
-
-	// toString
-	pl.type.JSValue.prototype.toString = function() {
-		return "JSValue" + (typeof this.value).toUpperCase();
-	};
-
-	// clone
-	pl.type.JSValue.prototype.clone = function() {
-		return new pl.type.JSValue( this.value );
-	};
-
-	// equals
-	pl.type.JSValue.prototype.equals = function( obj ) {
-		return pl.type.is_js_object( obj ) && this.value === obj.value;
-	};
-
-	// rename
-	pl.type.JSValue.prototype.rename = function( _ ) {
-		return this;
-	};
-
-	// get variables
-	pl.type.JSValue.prototype.variables = function() {
-		return [];
-	};
-
-	// apply substitutions
-	pl.type.JSValue.prototype.apply = function( _ ) {
-		return this;
-	};
-
-	// unify
-	pl.type.JSValue.prototype.unify = function( obj, _ ) {
-		if( pl.type.is_js_object( obj ) && this.value === obj.value ) {
-			return new pl.type.State( obj, new pl.type.Substitution() );
-		}
-		return null;
-	};
-
-	// interpret
-	pl.type.JSValue.prototype.interpret = function( indicator ) {
-		return pl.error.instantiation( indicator );
-	};
-
-	// compare
-	pl.type.JSValue.prototype.compare = function( obj ) {
-		if( this.value === obj.value ) {
-			return 0;
-		} else if( this.value < obj.value ) {
-			return -1;
-		} else if( this.value > obj.value ) {
-			return 1;
-		}
-	};
-	
-	// to javascript
-	pl.type.JSValue.prototype.toJavaScript = function() {
-		return this.value;
-	};
-	
-	// from javascrip
-	pl.fromJavaScript.conversion.any = function( obj ) {
-		return new pl.type.JSValue( obj );
-	};
-	
-	
-	
-	// JavaScript error
-	pl.error.javascript = function( error, indicator ) {
-		return new pl.type.Term( "error", [new pl.type.Term( "javascript_error", [new pl.type.Term( error )] ), pl.utils.str_indicator( indicator )] );
-	};
-	
-	
 
 
 	if( typeof module !== 'undefined' ) {
